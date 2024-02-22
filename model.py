ed992e54ed3ab6d8f40ba70606065ac550454660
Parvathy1122 <141606572+Parvathy1122@users.noreply.github.com>
2024-02-22 13:17:49 +0530

    Update model.py

--- model.py
+++ model.py
@@ -69,6 +69,49 @@ print(f'Number of class 0 in resampled training data: {sum(y_train==0)}')
 print(f'Number of class 1 in resampled training data: {sum(y_train==1)}\n')
 print(f'Number of class 2 in resampled training data: {sum(y_train==2)}\n')
 
+def evaluate_model(y_test,y_pred):
+    print("Classification Report")
+    print(classification_report(y_test, y_pred))
+
+    print("\n---------------------------------------------\n")
+    # Compute confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    # Create a heatmap of the confusion matrix using Seaborn
+    sns.heatmap(cm, annot=True, cmap='Greens',fmt='.0f')
+
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+
+    plt.show()
+
+#cross validation
+# List of classifiers to test
+classifiers = [
+    ('Decision Tree', DecisionTreeClassifier()),
+    ('Random Forest', RandomForestClassifier()),
+    #('KNN', KNeighborsClassifier(n_neighbors=5)),
+    #('Logistic regresssion', LogisticRegression())
+]
+
+# Iterate over each classifier and evaluate performance 
+for clf_name, clf in classifiers:
+    # Perform cross-validation
+    scores = cross_val_score(clf, X_train, y_train, cv=5, scoring='accuracy')
+
+    # Calculate average performance metrics
+    avg_accuracy = scores.mean()
+    avg_precision = cross_val_score(clf, X_train, y_train, cv=5, scoring='precision_macro').mean()
+    avg_recall = cross_val_score(clf, X_train, y_train, cv=5, scoring='recall_macro').mean()
+
+    # Print the performance metrics
+    print(f'Classifier: {clf_name}')
+    print(f'Average Accuracy: {avg_accuracy:.4f}')
+    print(f'Average Precision: {avg_precision:.4f}')
+    print(f'Average Recall: {avg_recall:.4f}')
+    print('-----------------------')
+
 # Creating the Random Forest classifier
 model_rf = RandomForestClassifier(random_state=42)
 
@@ -79,5 +122,40 @@ model_rf.fit(X_train, y_train)
 y_pred = model_rf.predict(X_test)
 accuracy = round(accuracy_score(y_test, y_pred), 4)
 print(accuracy)
+
+#hyperparameter
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+# Number of trees in random forest
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 800, num = 10)]
+# Number of features to consider at every split
+max_features = ['sqrt','log2']
+# Maximum number of levels in tree
+max_depth = [int(x) for x in np.linspace(1,40,10)]
+# Minimum number of samples required to split a node
+min_samples_split = [2, 5, 10 ,15]
+# Minimum number of samples required at each leaf node
+min_samples_leaf = [1, 2, 4 ]
+# Create the random grid
+random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_split': min_samples_split,
+               'min_samples_leaf': min_samples_leaf,
+              'criterion':['entropy','gini']}
+print(random_grid)
+rf_randomcv=RandomizedSearchCV(estimator=model_rf,param_distributions=random_grid,n_iter=10, cv=5, scoring='accuracy',random_state=42)
+### fit the randomized model
+rf_randomcv.fit(X_train,y_train)
+rf_randomcv.best_params_
+best_random_grid=rf_randomcv.best_estimator_
+from sklearn.metrics import accuracy_score
+y_pred=best_random_grid.predict(X_test)
+print(confusion_matrix(y_test,y_pred))
+print("Accuracy Score {}".format(accuracy_score(y_test,y_pred)))
+print("Classification report: {}".format(classification_report(y_test,y_pred)))
+model_rf=rf_randomcv
+
+
 #pickle.dump(model_rf, open('model23.pkl', 'wb'))
 joblib.dump(model_rf, 'credmodel.joblib')
